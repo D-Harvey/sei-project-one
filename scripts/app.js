283139b5e16a0d16a535500796228f509541db89
Davidh <davidharvey_@hotmail.com>
2021-11-13 13:47:30 +0000

    gameboard tweaked and added pacman image

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,10 @@
 
-const grid = document.querySelector('.game-board')
-console.log(grid)
+const grid = document.querySelector('.game-grid') 
 
 const width = 20
 const height = 30
 const gameGrid = width * height
 const cells = []
-
-console.log(gameGrid)
 
 function buildTheGameGrid () {
   for (let i = 0; i < gameGrid; i++) {
@@ -19,5 +16,6 @@ function buildTheGameGrid () {
 }
 
 buildTheGameGrid()
-
+console.log(typeof cells[1])
+cells[1].classList.add('pacman')
 console.log(cells)
